af9a3f0969d5135944edb087652ed01000356604
Kelsey Florek <kelseyflorek22@gmail.com>
2020-05-29 19:29:45 -0500

    fixed linking issue

--- electronDesktop/electronApp.js
+++ electronDesktop/electronApp.js
@@ -14,11 +14,21 @@ auspice.run(args);
 let mainWindow = null
 
 function main() {
-  mainWindow = new BrowserWindow({ width: 1024, height: 800, show: true })
-  mainWindow.loadURL(`http://localhost:4000/`)
+  mainWindow = new BrowserWindow({ width: 1024, height: 800, show: true });
+  mainWindow.loadURL(`http://localhost:4000/`);
+  //catch links that navigate us away from the app and send them to the default browser
+  mainWindow.webContents.on('will-navigate', function(e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+  mainWindow.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
+  
   mainWindow.on('close', event => {
     mainWindow = null
-  })
+  });
 }
 
 app.on('ready',main);
